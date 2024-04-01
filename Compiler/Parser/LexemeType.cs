6765c4a4138a3a0c116a93fdb89f4bbb7c4e2099
e <egor@mba.local>
2024-04-01 11:08:19 +0700

    update LexemT

--- Compiler/Parser/LexemeType.cs
+++ Compiler/Parser/LexemeType.cs
@@ -2,18 +2,18 @@ namespace Compiler.Parser;
 
 public enum LexemeType
 {
-    FnKeyword = 1,
-    I32Keyword = 2,
-    ReturnKeyword = 3,
-    Colon = 4,
-    Comma = 5,
-    OpenBracket = 6,
-    CloseBracket = 7,
-    OpenFnBody = 8,
-    CloseFnBody = 9,
-    Minus = 10,
-    Greater = 11,
-    Identifier = 12,
-    Semicolon = 13,
-    Separator = 14
+    FnKeyword,
+    ReturnKeyword,
+    I32Keyword,
+    Identifier,
+    OpenBracket,
+    Colon,
+    Comma,
+    CloseBracket,
+    Minus,
+    Greater,
+    OpenFnBody,
+    Semicolon,
+    CloseFnBody,
+    Separator
 }
